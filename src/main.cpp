5e805574b94ac54054323f6e4fccb0fb8b0f79f3
Vitalij35 <kostenkovitalij35@gmail.com>
2025-11-25 20:31:32 +0200

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// DEFINES
+#define BL_RX 10
+#define BL_TX 11
+#define MOT1_A 1
+#define MOT1_B 2
+#define MOT2_A 3
+#define MOT2_B 4
+#define MOT3_A 5
+#define MOT3_B 6
+#define RMOT_A 7
+#define RMOT_B 8
+// DEFINES
+
+// LIBS
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(BL_RX, BL_TX);
+
+#include <GyverMotor2.h>
+#define GMOTOR2_NO_ACCEL
+GMotor2<DRIVER2WIRE> mot1(MOT1_A, MOT1_B);
+GMotor2<DRIVER2WIRE> mot2(MOT2_A, MOT2_B);
+GMotor2<DRIVER2WIRE> mot3(MOT3_A, MOT3_B);
+GMotor2<DRIVER2WIRE> ramp_motor(RMOT_A, RMOT_B);
+// LIBS
+
+// VARS
+// VARS
+
+// function declarations
+void StopMotors();
+void SetMotorsXY(byte, byte, byte);
+// function declarations 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  bluetooth.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// function definitions
+void SetMotorsXY(byte x, byte y, byte R) {
+byte W1 = -0.5*x - sqrt(3)/2*y + R;
+byte W2 = -0.5*x + sqrt(3)/2*y + R;
+byte W3 = x + R;
+
+
 }
