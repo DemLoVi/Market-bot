53a30e65a95483328352e6ec5e8b80a3963f4848
Vitalij35 <kostenkovitalij35@gmail.com>
2025-12-03 16:08:55 +0200

    maybe working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
-// DEFINES
+
+/*==========DEFINES==========*/
 #define BL_RX 10
 #define BL_TX 11
 #define MOT1_A 1
@@ -11,11 +12,12 @@
 #define MOT3_B 6
 #define RMOT_A 7
 #define RMOT_B 8
-// DEFINES
+/*==========DEFINES==========*/
 
-// LIBS
+
+/*==========LIBS==========*/
 #include <SoftwareSerial.h>
-SoftwareSerial bluetooth(BL_RX, BL_TX);
+SoftwareSerial BTserial(BL_RX, BL_TX);
 
 #include <GyverMotor2.h>
 #define GMOTOR2_NO_ACCEL
@@ -23,29 +25,70 @@ GMotor2<DRIVER2WIRE> mot1(MOT1_A, MOT1_B);
 GMotor2<DRIVER2WIRE> mot2(MOT2_A, MOT2_B);
 GMotor2<DRIVER2WIRE> mot3(MOT3_A, MOT3_B);
 GMotor2<DRIVER2WIRE> ramp_motor(RMOT_A, RMOT_B);
-// LIBS
+/*==========LIBS==========*/
 
-// VARS
-// VARS
 
-// function declarations
+/*==========VARS==========*/
+bool doneParsing, startParsing;
+int16_t dataX, dataY;
+String string_convert;
+/*==========VARS==========*/
+
+
+/*==========FUNC-DECL==========*/
+bool parsing();
 void StopMotors();
 void SetMotorsXY(byte, byte, byte);
-// function declarations 
+/*==========FUNC-DECL==========*/
+
 
 void setup() {
-  Serial.begin(9600);
-  bluetooth.begin(9600);
+  // Serial.begin(9600);
+  BTserial.begin(9600);
 }
 
 void loop() {
+  if (parsing()) SetMotorsXY(dataX, dataY, 0); // if done parsing set motors
 }
 
-// function definitions
-void SetMotorsXY(byte x, byte y, byte R) {
-byte W1 = -0.5*x - sqrt(3)/2*y + R;
+
+/*==========FUNC-DEF==========*/
+void SetMotorsXY(byte x, byte y, byte R) { // set speeds to motors
+byte W1 = -0.5*x - sqrt(3)/2*y + R;        // convertation for qiwi drive
 byte W2 = -0.5*x + sqrt(3)/2*y + R;
 byte W3 = x + R;
 
+mot1.setSpeed(W1);                         // set motors
+mot2.setSpeed(W2);
+mot3.setSpeed(W3);
+}
 
+void StopMotors() { // full stop platform
+  mot1.stop();
+  mot2.stop();
+  mot3.stop();
+}
+
+bool parsing() {
+  if (BTserial.available() > 0) {        // if data avaible
+    char incomingChar = BTserial.read(); // read incoming data
+    if (startParsing) {                  // start parsing
+      if (incomingChar == ' ') {         // if we done reciving X data
+        dataX = string_convert.toInt();  // save X data
+        string_convert = "";             // clear buffer
+      }
+      else if (incomingChar == ';') {    // if we done reciving Y data
+        dataY = string_convert.toInt();  // save Y data
+        string_convert = "";             // clear buffer
+        startParsing = false;            // stop parsing
+        return true;                     // raturn done parsing
+      } else {
+        string_convert += incomingChar;  // add incoming symbol to buffer
+      }
+    }
+    if (incomingChar == '$') {           // start parsing
+      startParsing = true;              
+    }
+  }
+  return false;                          // return parsing not done
 }
